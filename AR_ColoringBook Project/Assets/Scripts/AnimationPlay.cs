e5239069905f4608fcca09fda5875995af095158
Akshay Makkar <akshaym1103@gmail.com>
2019-06-19 18:06:36 +0530

    Day 3 completed. Tiger UV modification remaining

---
+++ AR_ColoringBook Project/Assets/Scripts/AnimationPlay.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationPlay : MonoBehaviour
+{
+    public GameObject anim;
+    public void playAni()
+    {
+        Animator ani = anim.GetComponent<Animator>();
+        ani.SetInteger("dino",1);
+    }
+}
